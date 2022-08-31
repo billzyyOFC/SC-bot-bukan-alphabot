5565af577077f429c14749a5836039d426f85ac8
billzyyOFC <111672209+billzyyOFC@users.noreply.github.com>
2022-08-31 20:46:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6285220625571','6285220625571','6285220625571','6285220625571']
+global.premium = ['6285220625571']
+global.packname = 'Dikaaa x billzyy BOTZ'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
